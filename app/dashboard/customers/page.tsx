180adc51a69982ae5b630a748c816d0dd1b2168a
Ana Tripunoska Zdravkovikj <ana.zdravkovikj@valtech.com>
2025-02-21 11:56:18 +0100

    add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function CustomersPage() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import Table from "@/app/ui/customers/table";
+import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import Pagination from "@/app/ui/invoices/pagination";
+import { InvoicesTableSkeleton, TableRowSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import FormattedCustomersTable from "@/app/ui/customers/table";
+export default async function CustomersPage(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+        <FormattedCustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
